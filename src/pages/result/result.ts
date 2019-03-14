203b0fe32a6da23c231cb705a88a6bae53636153
Suvijak <yesdedoo@gmail.com>
2019-03-14 17:16:26 +0700

    Get Split variable result

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -5,6 +5,7 @@ import { ResulthistpercPage } from '../resulthistperc/resulthistperc';
 //REST API
 import { from } from 'rxjs/observable/from';
 import { TestapiProvider } from '../../providers/testapi/testapi';
+import { stringify } from '@angular/core/src/render3/util';
 
 /**
  * Generated class for the ResultPage page.
@@ -19,8 +20,16 @@ import { TestapiProvider } from '../../providers/testapi/testapi';
   templateUrl: 'result.html',
 })
 export class ResultPage {
+  
+  
   CourseInfo:any;
   Stu_ID:any;
+
+  Course="";
+  Course_ID:any;
+  Course_Code:any;
+  Course_Name:any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams,public testapiProvider: TestapiProvider) {
 
     this.Stu_ID = this.navParams.get('Stu_ID');
@@ -32,8 +41,52 @@ export class ResultPage {
     this.CourseInfo = from(this.testapiProvider.DisplayCourse(this.Stu_ID));
     this.CourseInfo.subscribe(val =>{
       console.log(val)
+
+      var ParsedVal = JSON.stringify(val)
+      var root = JSON.parse(ParsedVal)
+      console.log(ParsedVal)
+       
+      var loopcheck = 0;
+      for(var i in root[1]){
+        if(loopcheck==0){
+          this.Course_Code=root[1][i]
+        }
+        else if(loopcheck==1){
+          this.Course_ID=root[1][i]
+        }
+        else{
+          this.Course_Name = root[1][i]
+        }
+        loopcheck++;
+        console.log(root[1][i])
+      }
+      console.log(this.Course_Code,this.Course_ID,this.Course_Name)
+      var PCID = JSON.stringify(this.Course_ID)
+      var PCCODE = JSON.stringify(this.Course_Code)
+      var PCNAME = JSON.stringify(this.Course_Name)
+
+      PCCODE = PCCODE.substring(1,PCCODE.length-1)
+      PCID = PCID.substring(1,PCID.length-1)
+      PCNAME = PCNAME.substring(1,PCNAME.length-1)
+      console.log(PCID,PCCODE,PCNAME)
+      var SplitPCID  = PCID.split(",");
+      var SplitPCCODE = PCCODE.split(",");
+      var SplitPCNAME = PCNAME.split(",");
+      console.log(SplitPCID[0])
+
+      var conditionLength = SplitPCID.length
+      
+      for(var j=0; j<=conditionLength;j++){
+
+        this.Course = this.Course.concat(SplitPCID[j]) 
+        console.log(this.Course)
+        j++;      
+      }
     })
+    
   }
+
+  
 
   Database()
   {
